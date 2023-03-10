649794407f6816668f4369123bcfb27acc1f1bdf
José Soliz <solizcamposj@gmx.es>
2023-03-10 20:36:54 +0100

    Varias barajas

---
+++ blackjack.py
@@ -0,0 +1,36 @@
+import random
+#import sys
+#import time
+
+""" def mas_cartas():
+    print("¿El jugador quiere otra carta? Si/No", input())
+    if input() == "Si":
+        random.shuffle(deck)
+        print("El jugador recibe: ",deck[0])
+        mano_jugador 
+
+    return """
+
+picas = treboles = diamantes = corazones = [1,2,3,4,5,6,7,8,9,10,11]
+
+deck = [picas, treboles, diamantes, corazones]    
+deck1 = [picas, treboles, diamantes, corazones]    
+deck2 = [picas, treboles, diamantes, corazones]    
+deck3 = [picas, treboles, diamantes, corazones]    
+deck4 = [picas, treboles, diamantes, corazones]    
+deck5 = [picas, treboles, diamantes, corazones]    
+
+decks = [deck, deck1, deck2,deck3, deck4, deck5]
+
+
+
+""" mano_jugador = deck [0]    
+print("El jugador recibe: ",deck[0])
+
+random.shuffle(deck)
+mano_jugador += deck[2]
+
+print("El jugador recibe: ",deck[2])
+print("Total de la mano del jugador: ",mano_jugador) """
+
+#mas_cartas()
