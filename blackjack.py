a2cda4c8adbd1f2d7171d87143e42232202612be
José Soliz <solizcamposj@gmx.es>
2023-03-10 20:45:15 +0100

    Baraja mejorada

--- blackjack.py
+++ blackjack.py
@@ -11,26 +11,19 @@ import random
 
     return """
 
-picas = treboles = diamantes = corazones = [1,2,3,4,5,6,7,8,9,10,11]
+deck = [1,2,3,4,5,6,7,8,9,10,11]    
 
-deck = [picas, treboles, diamantes, corazones]    
-deck1 = [picas, treboles, diamantes, corazones]    
-deck2 = [picas, treboles, diamantes, corazones]    
-deck3 = [picas, treboles, diamantes, corazones]    
-deck4 = [picas, treboles, diamantes, corazones]    
-deck5 = [picas, treboles, diamantes, corazones]    
-
-decks = [deck, deck1, deck2,deck3, deck4, deck5]
-
-
-
-""" mano_jugador = deck [0]    
-print("El jugador recibe: ",deck[0])
+pack = ["picas", "treboles", "diamantes", "corazones"]
+random.shuffle(deck)
+random.shuffle(pack)
+mano_jugador = deck [0]    
+print("El jugador recibe: ",deck[0] + pack)
 
 random.shuffle(deck)
+random.shuffle(pack)
 mano_jugador += deck[2]
 
-print("El jugador recibe: ",deck[2])
-print("Total de la mano del jugador: ",mano_jugador) """
+print("El jugador recibe: ",deck[2] + pack)
+print("Total de la mano del jugador: ",mano_jugador)
 
 #mas_cartas()
