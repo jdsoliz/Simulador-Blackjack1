d038e35fe31072c986497ea54085086394871983
José Soliz <solizcamposj@gmx.es>
2023-03-14 17:35:50 +0100

    Cambios en la mano resueltos

--- blackjack.py
+++ blackjack.py
@@ -2,28 +2,32 @@ import random
 #import sys
 #import time
 
-""" def mas_cartas():
-    print("¿El jugador quiere otra carta? Si/No", input())
-    if input() == "Si":
-        random.shuffle(deck)
-        print("El jugador recibe: ",deck[0])
-        mano_jugador 
+def mas_cartas():
+    print("El jugador recibe: ",deck[0])
+    mano_jugador
 
-    return """
+    return
 
 deck = [1,2,3,4,5,6,7,8,9,10,11]    
-
 pack = ["picas", "treboles", "diamantes", "corazones"]
-random.shuffle(deck)
-random.shuffle(pack)
-mano_jugador = deck [0]    
-print("El jugador recibe: ",deck[0] + pack)
 
 random.shuffle(deck)
 random.shuffle(pack)
+
+mano_jugador = deck [0]    
+
+print('El jugador recibe:',deck[0], 'de', pack[0])
+
+random.shuffle(deck)
+random.shuffle(pack)
+
 mano_jugador += deck[2]
 
-print("El jugador recibe: ",deck[2] + pack)
+print('El jugador recibe:',deck[2], 'de', pack[2])
 print("Total de la mano del jugador: ",mano_jugador)
 
-#mas_cartas()
+print("¿El jugador quiere otra carta? Si/No", input())
+if input() == "Si":
+    random.shuffle(deck)
+    mas_cartas() 
+
